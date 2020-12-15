196885005a7193be123b4e0eda3f3555d05576f5
meshare <845545348@qq.com>
2020-12-15 19:53:27 +0800

    - Fix: 命令行 `format` 选项错误处理

--- src/command.ts
+++ src/command.ts
@@ -5,24 +5,36 @@ import commander from 'commander';
 import pkg from '../package.json';
 import execute from './execute';
 
-commander
-    .storeOptionsAsProperties(false)
-    .passCommandToAction(false)
-    .name('j-icon')
-    .version(pkg.version)
-    .description(pkg.description)
-    .allowUnknownOption(true)
-    .requiredOption('-i, --input <path>', 'A file or directory path')
-    .requiredOption('-o, --output <path>', 'File output directory')
-    .option('-m, --minify', 'Compressed file', false)
-    .option<string[]>('-f, --format [formats...]', 'Output file format', value => value.split(/\s+/), ['json', 'esm', 'umd', 'ts', 'type'])
-    .option<number>('-t, --target [version]', 'Target Vue version, 2 or 3', value => Number.parseInt(value, 10), 3)
-    .option('-n, --name [name]', 'Output file name', 'index')
-    .parse(process.argv);
+const defaultFormats = ['json', 'esm', 'umd', 'ts', 'type'];
 
 function normalizePath(path: string): string {
     return PATH.isAbsolute(path) ? path : PATH.join(process.cwd(), path);
 }
+
+function executeFormat(value: string, prev: string[]): string[] {
+    if (prev.length === defaultFormats.length
+        && prev.every((val, i) => val === defaultFormats[i])) {
+        return [value];
+    }
+
+    prev.push(value);
+    return prev;
+}
+
+commander
+    .storeOptionsAsProperties(false)
+    .passCommandToAction(false)
+    .allowUnknownOption(true)
+    .name('j-icon')
+    .version(pkg.version)
+    .description(pkg.description)
+    .requiredOption('-i, --input <path>', 'A file or directory path')
+    .requiredOption('-o, --output <path>', 'File output directory')
+    .option('-m, --minify', 'Compressed file', false)
+    .option<string[]>('-f, --format [formats...]', 'Output file format', executeFormat, defaultFormats)
+    .option<number>('-t, --target [version]', 'Target Vue version, 2 or 3', value => Number.parseInt(value, 10), 3)
+    .option('-n, --name [name]', 'Output file name', 'index')
+    .parse(process.argv);
 
 (async () => {
     const opts = commander.opts();
